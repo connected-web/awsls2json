76b041d68597f04ed1f702b45f072ed16c8d8925
John Beech <john.beech@bbc.co.uk>
2016-11-10 17:03:12 +0000

    Support folders returned by aws s3 ls

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ var NL = '\n';
 
 // Example aws s3 ls line:
 // 2016-09-15 13:51:24        258 a7a7906.json
-var matcher = /(\d\d\d\d-\d\d-\d\d)\s+(\d\d:\d\d:\d\d)\s+(\d+)\s+(.*)/;
+var fileMatcher = /(\d\d\d\d-\d\d-\d\d)\s+(\d\d:\d\d:\d\d)\s+(\d+)\s+(.*)/;
+var folderMatcher = /(PRE)\s+(.*\/)/;
 
 function decodeLine(line) {
   var result = {};
 
-  if (matcher.test(line)) {
-    var parts = line.match(matcher);
+  if (fileMatcher.test(line)) {
+    var parts = line.match(fileMatcher);
     result.date = parts[1];
     result.time = parts[2];
     result.size = parts[3];
     result.file = parts[4];
+  } else if (folderMatcher.test(line)) {
+    var parts = line.match(folderMatcher);
+    result.pre = parts[1];
+    result.folder = parts[2];
   } else {
     result = false;
   }
--- test/convert-spec.js
+++ test/convert-spec.js
@@ -1,12 +1,20 @@
 const expect = require('chai').expect;
-const filelist = require('fs').readFileSync(__dirname + '/fixtures/filelist.txt', 'utf8');
-
 const convert = require('../index');
-const expected = require('./fixtures/filelist.json');
 
 describe('Convert', () => {
   it('should convert aws ls s3 output into JSON', () => {
+    const filelist = require('fs').readFileSync(__dirname + '/fixtures/filelist.txt', 'utf8');
+    const expected = require('./fixtures/filelist.json');
+
     const actual = convert(filelist);
+    expect(actual).to.deep.equal(expected);
+  });
+
+  it('should convert aws ls s3 output containing folders into JSON', () => {
+    const folderlist = require('fs').readFileSync(__dirname + '/fixtures/folderlist.txt', 'utf8');
+    const expected = require('./fixtures/folderlist.json');
+
+    const actual = convert(folderlist);
     expect(actual).to.deep.equal(expected);
   });
 });
